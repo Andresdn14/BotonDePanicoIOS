f7cce97b8102e9881cd9146a3f00625501daf352
Andres Felipe De La Ossa Navarro <andresdelaossa@hotmail.com>
2019-05-29 12:30:21 -0500

    Almost finished app

---
+++ BotonDePanicoIOS/AboutUsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AboutUsViewController.swift
+//  BotonDePanicoIOS
+//
+//  Created by Andres Felipe De La Ossa Navarro on 5/20/19.
+//  Copyright © 2019 Andres Felipe De La Ossa Navarro. All rights reserved.
+//
+
+import UIKit
+
+class AboutUsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
